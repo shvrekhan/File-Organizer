dd3a71bcf1b4439834fe3d979c7d56ad64f8e8e6
shvrekhan <shvrekhan@gmail.com>
2022-08-02 16:58:37 +0530

    utility.js implemented

---
+++ utility.js
@@ -0,0 +1,9 @@
+utility = {};
+utility.types = {
+        media :["mp4","mkv"],
+        archives:['zip','7z','rar','tar','gz','ar','iso','xz'],
+        documents:['docx','doc','pdf','xlsx','xls','odt','ods','txt','odf'],
+        app : ['exe','dmg','pkg','deb']
+}
+
+module.export = utility;
